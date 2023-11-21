16f7a703bb1d0ef7a22b04228d2c930161ff0cbd
Chinyere <chinyereunamba17@gmail.com>
2023-11-21 13:25:17 +0100

    added functionality for trial message

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+const setupIcon = document.querySelector(".icon")
+const openCloseIcon = document.querySelectorAll(".icon span")
+const setupGuideContent = document.querySelector('.content')
+const trialClose = document.querySelector('.trial-msg .close-icon')
+const trialMsg = document.querySelector('.trial-msg')
+const textContent = document.querySelectorAll('.content .text-content')
 
-// Your javascript goes here
+trialClose.addEventListener('click', () => {
+    trialMsg.classList.add('close')
+})
+
+setupIcon.addEventListener("click", () => {
+    openCloseIcon.forEach((btn) => {
+        btn.classList.toggle("active")
+    })
+    setupGuideContent.classList.toggle('active')
+})
