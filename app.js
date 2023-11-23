d481fcab51c0df3e94e6f34a232a7e13b56e781f
Chinyere <chinyereunamba17@gmail.com>
2023-11-23 08:52:00 +0100

    done with all functionality

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const notifyIcon = document.querySelector(".notify-icon")
 const profileIcon = document.querySelector(".profile-icon")
 const notifyDialog = document.querySelector(".notify-dialog")
 const profileDialog = document.querySelector(".profile-dialog")
-const setupIcon = document.querySelector(".setup button.icon")
+const setupIcon = document.querySelector(".setup button.setup-icon")
 const openCloseIcon = document.querySelectorAll(".icon span")
 const setupGuideContent = document.querySelector(".content")
 const setupIntro = document.querySelector(".setup-guide .intro")
 const trialClose = document.querySelector(".trial-msg .close-icon")
 const trialMsg = document.querySelector(".trial-msg")
 const list = document.querySelectorAll(".content li")
-const checkMark = document.querySelectorAll(".content span.icon")
+const checkMark = document.querySelectorAll(".content button.icon")
+const checkMarkVariants = document.querySelectorAll(".content button.icon svg")
 const title = document.querySelectorAll(".content .title")
 const textContent = document.querySelectorAll(".content .text-content")
 const imageContent = document.querySelectorAll(".content .image-content")
+const bar = document.querySelector(".bar")
 
 notifyIcon.addEventListener("click", () => {
   notifyIcon.classList.toggle("active")
@@ -62,6 +64,10 @@ setupIntro.addEventListener("click", () => {
   openCloseIcon[1].classList.remove("active")
 })
 
+let barValue = 0
+const progressLabel = document.querySelector(".progress .label")
+const styleElem = document.head.appendChild(document.createElement("style"))
+
 list.forEach((el, index) => {
   title[index].addEventListener("click", () => {
     list.forEach((item) => {
@@ -69,7 +75,26 @@ list.forEach((el, index) => {
     })
     el.classList.toggle("active")
   })
+  title[index].addEventListener("keydown", () => {
+    list.forEach((item) => {
+      item.classList.remove("active")
+    })
+    el.classList.toggle("active")
+  })
   checkMark[index].addEventListener("click", () => {
-    console.log("clicked")
+    checkMark[index].children.item(1).classList.toggle("active")
+    checkMark[index].children.item(0).classList.toggle("active")
+    if (checkMark[index].children.item(1).classList.contains("active")) {
+      list.forEach((item) => {
+        item.classList.remove("active")
+      })
+      list[index < 4 ? index + 1 : index].classList.add("active")
+    }
+    checkMark[index].children.item(0).classList.contains("active")
+    ? barValue--
+    : barValue++
+    
+    styleElem.innerHTML = `.progress .bar::after {width: ${14.4 * barValue}px;}`
+    progressLabel.textContent = `${barValue} / 5 completed`
   })
 })
